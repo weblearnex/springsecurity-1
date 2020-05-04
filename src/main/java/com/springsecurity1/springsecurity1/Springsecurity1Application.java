6b9dd84c655e8e3dcb5ce947d9a2c85168516200
virendra1984 <virendrams2014@gmail.com>
2020-05-05 01:52:40 +0530

    first commit

---
+++ src/main/java/com/springsecurity1/springsecurity1/Springsecurity1Application.java
@@ -0,0 +1,13 @@
+package com.springsecurity1.springsecurity1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//Spring simple security
+@SpringBootApplication
+public class Springsecurity1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springsecurity1Application.class, args);
+	}
+
+}
